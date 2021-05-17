86a0cd0c799cf418675171282bb409f335d36017
isildur13 <panchalyash13@gmail.com>
2021-05-17 21:00:05 +0530

    uninstaller

---
+++ automator/fsrepl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+inotifywait -m /home/yash/java/ -e create -e moved_to |
+    while read dir action file; do
+        echo "The file '$file' appeared in directory '$dir' via '$action'"
+
+	echo "Building new image"
+        cd /home/yash/java/ && ./builder.sh
+
+        # do something with the file
+    done
+
---
+++ automator/uninstall-service.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+systemctl disable fsrep
+
+rm  /etc/systemd/system/fsrep.service 
+
+rm /usr/bin/fsrepl.sh
+
+
